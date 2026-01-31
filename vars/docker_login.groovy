0c605e00a3f3b16eb5264a952145c0c8b0d8d192
Harshit Rai <155638485+Harshitraiii2005@users.noreply.github.com>
2026-02-01 00:01:46 +0530

    Implement Docker login function in Jenkins pipeline
    Add a Jenkins pipeline function for Docker login using credentials.

---
+++ vars/docker_login.groovy
@@ -0,0 +1,13 @@
+def call() {
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhub-creds',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+        sh '''
+        echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+        '''
+    }
+}
